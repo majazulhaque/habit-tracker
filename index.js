5f337c57b607907de6f59cf1d7cf21992e26fb11
Majaz <majazulhaque123@gmail.com>
2023-05-06 16:10:54 +0530

    path

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const express = require('express');
+const path = require('path');
 const port = process.env.PORT || 8000;
 const app = express();
 const ejs = require('ejs');
