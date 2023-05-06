f63d013494a788b445046569e5f7b133484e9ff0
Majaz <majazulhaque123@gmail.com>
2023-05-06 16:25:31 +0530

    commit

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const path = require('path');
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 3000;
 const app = express();
 const ejs = require('ejs');
 const expressLayouts = require('express-ejs-layouts');
