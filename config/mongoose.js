190224662328e2acc93603f0dc12161218aefce8
Majaz <majazulhaque123@gmail.com>
2023-05-07 11:08:30 +0530

    g

--- config/mongoose.js
+++ config/mongoose.js
@@ -2,8 +2,14 @@ const mongoose = require('mongoose');
 
 // const uri = process.env.HABIT_URI;
 
+mongoose.connect('mongodb+srv://majazhaq98:RJZElqjwzCHvWQCO@cluster0.k5xlx9n.mongodb.net/test?retryWrites=true&w=majority', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  connectTimeoutMS: 30000,
+  socketTimeoutMS: 30000
+});
 
-mongoose.connect(process.env.MONGO_URI);
+// mongoose.connect(process.env.MONGO_URI);
 const db = mongoose.connection;
 
 db.on('error', console.error.bind(console, 'DB Error'));
